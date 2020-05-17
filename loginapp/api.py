8b12cfa28440cf7c1ea1b056224958189d130519
Lei <gabriel_sl19798@hotmail.com>
2020-05-17 23:29:42 +1200

    login testing

--- loginapp/api.py
+++ loginapp/api.py
@@ -25,7 +25,7 @@ class LoginAPI(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validate_data
+        user = serializer.validated_data
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
