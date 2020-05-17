43265f7bbbeb3f553622771f9f0e6bf0bbf8640c
Lei <gabriel_sl19798@hotmail.com>
2020-05-17 22:51:36 +1200

    register testing

--- loginapp/serializers.py
+++ loginapp/serializers.py
@@ -14,7 +14,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password':{'Write_only':True}}
+        extra_kwargs = {'password':{'write_only':True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
